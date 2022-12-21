be447735bc01b331f6648e2e1eb069123c2eb266
Kundai Kwangwari <61218439+Kundai-Kwangwari@users.noreply.github.com>
2022-12-21 12:46:39 +0800

    Initial commit for FGPFPMiner base algorithm

--- FuzzyGeoreferencedPeriodicFrequentPatternMiner_base_algorithm/abstract.py
+++ FuzzyGeoreferencedPeriodicFrequentPatternMiner_base_algorithm/abstract.py
@@ -1,5 +1,8 @@
 #  Copyright (C)  2021 Rage Uday Kiran
 #
+#  This program was written by Kundai Kwangwari and S.Chitra under the supervision
+#  of Professor Uday Kiran Rage
+#
 #      This program is free software: you can redistribute it and/or modify
 #      it under the terms of the GNU General Public License as published by
 #      the Free Software Foundation, either version 3 of the License, or
